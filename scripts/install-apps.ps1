6ee55c3eb4f9a9eae7a8c22edf8b2c53f458a7b8
Jamie Temple <jamie-temple@live.de>
2022-11-28 14:50:22 +0100

    feat: version 2.1

--- scripts/install-apps.ps1
+++ scripts/install-apps.ps1
@@ -1,9 +1,48 @@
-if (-Not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    throw "Not implemented."
+param(
+    [Parameter(Mandatory)]$File
+)
+
+$PackageManager = Get-Content -Path $File -TotalCount 1
+$Apps = Get-Content -Path $File -Exclude $PackageManager
+
+if ($PackageManager.ToLower() -eq "winget") {
+    if (-Not (Get-Command winget -ErrorAction SilentlyContinue)) {
+        Write-Host "Winget is not installed." -ForegroundColor Red
+        return
+    }
+
+    foreach ($item in $Apps) {
+        if ($item -notmatch $PackageManager -and $item -ne "") {
+            winget install `
+                --accept-package-agreements `
+                --accept-source-agreements `
+                -e --id $item
+        }
+    }
 }
-elseif (-Not (Get-Command scoop -ErrorAction SilentlyContinue)) {
-    throw "Not implemented."
+
+if ($PackageManager.ToLower() -eq "scoop") {
+    if (-Not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+        Write-Host "Scoop is not installed." -ForegroundColor Red
+        return    
+    }
+
+    scoop bucket add extras
+
+    foreach ($item in $Apps) {
+        if ($item -notmatch $PackageManager -and $item -ne "") {
+            scoop install $item
+        }
+    }
 }
+
+
+
+return
+
+
+
+
 else {
     Write-Host "Cannot install apps." -ForegroundColor Red
 }
